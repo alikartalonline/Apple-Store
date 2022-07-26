d0813057b5314c0dd9b942457ea0c0b1ec312366
Ali Kartal <ali_kartal91@hotmail.com>
2022-07-26 16:13:32 +0300

    update

--- src/components/HomePage/homepage.js
+++ src/components/HomePage/homepage.js
@@ -12,11 +12,8 @@ function HomePage() {
                 <figure className="mair-image">
 
                     <div className='posmac'>
-
                         <h2 className="headlinemair ">MacBook Air</h2>
-
                         <h3 className="logo-image logo-image-macbook-air-logo-hero-macbookair " role="text"></h3>
-
 
                         <div className="cta-links">
                             <a className="me-4" href="" >Daha fazla bilgi<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" /></a>
@@ -72,7 +69,7 @@ function HomePage() {
 
             <div className='d-flex section4pos'>
 
-                <div className='col-6 mbpro'>
+                <div className='col-6 mbpro me-2'>
                     <figure className='mbpro-image'>
                         <h4 class="headlinesection4" >13 inç MacBook Pro</h4>
                         <h5
@@ -80,14 +77,14 @@ function HomePage() {
                         >
                         </h5>
                         <div className="cta-links">
-                        <a className="me-4" 
-                        style={{fontSize:'1rem'}}
-                        href="" >Daha fazla bilgi<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" /></a>
+                            <a className="me-4"
+                                style={{ fontSize: '1rem' }}
+                                href="" >Daha fazla bilgi<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" /></a>
 
-                        <a 
-                        style={{fontSize:'1rem'}} 
-                        href="">Satın alın</a><img src="https://i.hizliresim.com/t2bcvmu.png" alt="" />
-                    </div>
+                            <a
+                                style={{ fontSize: '1rem' }}
+                                href="">Satın alın</a><img src="https://i.hizliresim.com/t2bcvmu.png" alt="" />
+                        </div>
                     </figure>
                 </div>
 
@@ -95,17 +92,59 @@ function HomePage() {
 
                 <div className='col-6'>
                     <figure className='ip13-image'>
-                    <h1 className='headlinei13'>iPhone 13</h1>
-                    <h3 class="subheadi13">Süper bir güç.</h3>
-                    <div className="cta-links">
-                        <a className="me-4" 
-                        style={{fontSize:'1rem'}}
-                        href="" >Daha fazla bilgi<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" /></a>
+                        <h1 className='headlinei13'>iPhone 13</h1>
+                        <h3 class="subheadi13">Süper bir güç.</h3>
+                        <div className="cta-links">
+                            <a className="me-4"
+                                style={{ fontSize: '1rem' }}
+                                href="" >Daha fazla bilgi<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" /></a>
 
-                        <a 
-                        style={{fontSize:'1rem'}} 
-                        href="">Satın alın</a><img src="https://i.hizliresim.com/t2bcvmu.png" alt="" />
-                    </div>
+                            <a
+                                style={{ fontSize: '1rem' }}
+                                href="">Satın alın</a><img src="https://i.hizliresim.com/t2bcvmu.png" alt="" />
+                        </div>
+                    </figure>
+                </div>
+
+            </div>
+
+
+            <div className='d-flex section4pos'>
+
+                <div className='col-6 me-2'>
+                    <figure className='i13pro-image'>
+                        <h1 className='headlinei13'>iPhone 13 Pro</h1>
+                        <h3 class="subheadi13">Pro. Aktif.</h3>
+                        <div className="cta-links">
+                            <a className="me-4"
+                                style={{ fontSize: '1rem' }}
+                                href="" >Daha fazla bilgi<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" /></a>
+
+                            <a
+                                style={{ fontSize: '1rem' }}
+                                href="">Satın alın</a><img src="https://i.hizliresim.com/t2bcvmu.png" alt="" />
+                        </div>
+                    </figure>
+                </div>
+
+
+
+                <div className='col-6'>
+                    <figure className='apods-image'>
+                        <div className='apodscontainer'>
+                            <h4 class="headlineapods">AirPods</h4>
+                            <h5 class="subheadapods">Uzamsal Ses teknolojisine sahip <br /> yepyeni tasarım.</h5>
+                            <div className="cta-links">
+                                <a className="me-4"
+                                    style={{ fontSize: '1rem' }}
+                                    href="" >Daha fazla bilgi<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" /></a>
+
+                                <a
+                                    style={{ fontSize: '1rem' }}
+                                    href="">Satın alın</a><img src="https://i.hizliresim.com/t2bcvmu.png" alt="" />
+                            </div>
+
+                        </div>
                     </figure>
                 </div>
 
@@ -114,7 +153,43 @@ function HomePage() {
 
 
 
+            <div className='d-flex section4pos'>
 
+                <div className='col-6 me-2'>
+                    <figure className='airtag-image'>
+                        <div className='airtagcontainer'>
+                            <h4 class="airtagheadline"></h4>
+                            <h5 class="subheadairtag">Hafif. Rengarenk. Gücüyle tek.</h5>
+                            <div className="cta-links">
+                                <a className="me-4"
+                                    style={{ fontSize: '1.1rem' }}
+                                    href="" >Daha fazla bilgi<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" />
+                                </a>
+
+                                <a
+                                    style={{ fontSize: '1.1rem' }}
+                                    href="">Satın alın<img src="https://i.hizliresim.com/t2bcvmu.png" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                    </figure>
+                </div>
+
+
+
+                <div className='col-6'>
+                    <figure className='ipadair-image'>
+                        <h4 className="ipadairheadline"></h4>
+                        <div className='musiclivecontainer'>
+                            <h5 className='subheadipadair'>Mary J. Blige'ın canlı performansını <br /> 27 Temmuz'da izleyin. <br /> Sadece Apple Music'te.</h5>
+                            <a className="text-white"
+                                style={{ fontSize: '1.1rem', textAlign: "center" }}
+                                href="" >Daha fazla bilgi<img className='' src="https://i.hizliresim.com/t2bcvmu.png" alt="" /></a>
+                        </div>
+                    </figure>
+                </div>
+
+            </div>
 
 
         </section>
