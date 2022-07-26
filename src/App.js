21a0ed80532f702df6edca2af8964efc1b37c2b9
Ali Kartal <ali_kartal91@hotmail.com>
2022-07-26 19:18:06 +0300

    footer added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Header from './components/Header/header';
 import HomePage from './components/HomePage/homepage';
-
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,213 @@
+import React from 'react';
+import './footer.css';
+
+
+function Footer() {
+
+    return (
+        <div className='footercontainer'>
+
+            <footer className='container'>
+                <div className='row '>
+
+                    <p className='col-12 footerheadline'>* Koşullara Uygun Müşteriler, Kampanya Dahilindeki Yerlerden kampanya dahilinde bir Mac veya kampanya dahilinde bir iPad ile birlikte kampanya dahilinde AirPods satın aldıklarında Kampanya İndirimi’nden yararlanabilirler. Kampanya dahilinde bir Mac veya kampanya dahilinde bir iPad satın alan Koşullara Uygun Müşteri başına yalnızca bir AirPods çifti için geçerlidir. Teklif stoklarla sınırlıdır. Burada belirtilen hüküm ve koşullara tabidir.
+
+                    </p>
+
+
+                    <div className='col'>
+                        <ul className='container'>
+                            <h3 class="directory-section-title">Alışveriş ve Detaylı Bilgi</h3>
+                            <li>
+                                <a href="">store</a>
+                            </li>
+                            <li>
+                                <a href="">Mac</a>
+                            </li>
+                            <li>
+                                <a href="">iPad</a>
+                            </li>
+                            <li>
+                                <a href="">iPhone</a>
+                            </li>
+                            <li>
+                                <a href="">Watch</a>
+                            </li>
+                            <li>
+                                <a href="">AirPods</a>
+                            </li>
+                            <li>
+                                <a href="">TV ve EV</a>
+                            </li>
+                            <li>
+                                <a href="">AirTag</a>
+                            </li>
+                            <li>
+                                <a href="">Aksesuarlar</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div className='col'>
+                        <ul className='container'>
+                            <h3 class="directory-section-title">Servisler</h3>
+                            <li>
+                                <a href="">Apple Music</a>
+                            </li>
+                            <li>
+                                <a href="">Apple Arcade</a>
+                            </li>
+                            <li>
+                                <a href="">iCloud</a>
+                            </li>
+                            <li>
+                                <a href="">Apple Books</a>
+                            </li>
+                            <li>
+                                <a href="">Apple Podcasts</a>
+                            </li>
+                            <li>
+                                <a href="">App Store</a>
+                            </li>
+                        </ul>
+
+                        <ul className='container' >
+                            <h3 class="directory-section-title mt-5">Hesap</h3>
+
+                            <li>
+                                <a href="">Apple ID'nizi Yönetin</a>
+                            </li>
+                            <li>
+                                <a href="">Apple Store Hesabı</a>
+                            </li>
+                            <li>
+                                <a href="">iCloud.com</a>
+                            </li>
+                        </ul>
+
+                    </div>
+
+
+                    <div className='col'>
+                        <ul className='container'>
+                            <h3 class="directory-section-title">Apple Store</h3>
+                            <li>
+                                <a href="">Mağaza Bulun</a>
+                            </li>
+                            <li>
+                                <a href="">Genius Bar</a>
+                            </li>
+                            <li>
+                                <a href="">Today at Apple</a>
+                            </li>
+                            <li>
+                                <a href="">Apple Yaz Kampı</a>
+                            </li>
+                            <li>
+                                <a href="">Apple Store Uygulaması</a>
+                            </li>
+                            <li>
+                                <a href="">Ödeme Kolaylıkları</a>
+                            </li>
+                            <li>
+                                <a href="">Apple Geri Dönüşüm Programı</a>
+                            </li>
+                            <li>
+                                <a href="">Sipariş Durumu</a>
+                            </li>
+                            <li>
+                                <a href="">Alışveriş Yardımı</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+
+                    <div className='col'>
+                        <ul className='container'>
+                            <h3 class="directory-section-title">Kurumsal Müşteriler İçin</h3>
+                            <li>
+                                <a href="">Apple ve İş Dünyası</a>
+                            </li>
+                            <li>
+                                <a href="">Şirketiniz için Alışveriş Yapın</a>
+                            </li>
+                        </ul>
+
+                        <ul className='container'>
+                            <h3 class="directory-section-title mt-5">Eğitim İçin</h3>
+                            <li>
+                                <a href="">Apple ve Eğitim</a>
+                            </li>
+                            <li>
+                                <a href="">K-12 için Alışveriş Yapın</a>
+                            </li>
+                            <li>
+                                <a href="">Üniversite için Alışveriş Yapın </a>
+                            </li>
+                        </ul>
+
+                    </div>
+
+
+
+                    <div className='col'>
+                        <ul className='container'>
+                            <h3 class="directory-section-title">Apple'ın Değerleri</h3>
+                            <li>
+                                <a href="">Erişebilirlik</a>
+                            </li>
+                            <li>
+                                <a href="">Çevre</a>
+                            </li>
+                            <li>
+                                <a href="">Gizlilik</a>
+                            </li>
+                            <li>
+                                <a href="">Tedarikçi Sorumluluğu</a>
+                            </li>
+                        </ul>
+
+                        <ul className='container'>
+                            <h3 class="directory-section-title mt-5"> Apple Hakkında</h3>
+                            <li>
+                                <a href="">Newsroom</a>
+                            </li>
+                            <li>
+                                <a href="">Apple Yönetimi</a>
+                            </li>
+                            <li>
+                                <a href="">Kariyer Fırsatları</a>
+                            </li>
+                            <li>
+                                <a href="">Garanti</a>
+                            </li>
+                            <li>
+                                <a href="">Etik ve Uyumluluk</a>
+                            </li>
+                            <li>
+                                <a href="">Etkinlikler</a>
+                            </li>
+                            <li>
+                                <a href="">Apple ile İletişim</a>
+                            </li>
+                        </ul>
+
+                    </div>
+
+                    <p className='col-12 footerbottom'>Diğer alışveriş seçenekleri: Yakınınızda bir Apple Store veya başka bir yetkili satıcı bulun. Veya 00800 448 829 873 ya da 0216 282 15 11 numaralı telefonu arayın.</p>
+
+
+
+
+
+
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+
+export default Footer;
