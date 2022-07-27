d78f1a56e394b9d9f0838aaa0619e0070feeafe6
Ali Kartal <ali_kartal91@hotmail.com>
2022-07-27 17:19:19 +0300

    update

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -13,9 +13,8 @@ function Header() {
                     <ul className='ac-gn-list row'>
                         <li className='col' >
                             <Link to='/'>
-                            <a className="ac-gn-link ac-gn-link-apple" href="" data-analytics-title="apple home" >
-                                {/* <span className="ac-gn-link-text">Apple</span> */}
-                            </a>
+                                <a className="ac-gn-link ac-gn-link-apple" href="" data-analytics-title="apple home" >
+                                </a>
                             </Link>
                         </li>
 
@@ -81,34 +80,87 @@ function Header() {
                             </a>
                         </li>
 
-                        {/* <a className=" ac-gn-link ac-gn-link-search" href="" data-analytics-title="store"> */}
-
                         <li className='col'>
-                            <a id="ac-gn-link-search" className="ac-gn-link ac-gn-link-search" href="" data-analytics-title="search" data-analytics-intrapage-link="" aria-label="apple.com/tr’de arayın" role="button" aria-haspopup="true"></a>
+                            <a id="ac-gn-link-search" className="ac-gn-link ac-gn-link-search" href="" data-analytics-title="search" aria-label="apple.com/tr’de arayın" role="button"></a>
                         </li>
 
-                        <li className="ac-gn-item ac-gn-bag col" id="ac-gn-bag">
-                            <div className="ac-gn-bag-wrapper dropdown ">
+                        <li className="col">
+                            <div className="ac-gn-bag-wrapper dropdown "
+                            
+                            >
                                 <a
-                                    className="ac-gn-link ac-gn-link-bag dropdown-toggle "
+                                    className="ac-gn-link ac-gn-link-bag  "
                                     href=""
                                     aria-label="Alışveriş Çantası"
                                     role="button"
-                                    aria-haspopup="true"
-                                    aria-expanded="false"
-                                    aria-controls="ac-gn-bagview-content"
                                     id="dropdownMenuLink"
                                     data-bs-toggle="dropdown"
                                 >
                                 </a>
 
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                    <li><p class="dropdown-item" >Alışveriş çantanız boş.</p></li>
-                                    <li><hr class="dropdown-divider" /></li>
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                    <li><p className="dropdown-item" 
+                                    style={{ padding: "20px", fontSize: "15px", textAlign:"center" }}
+                                    >
+                                        Alışveriş çantanız boş.
+                                    </p>
+                                    </li>
+
+                                    <li><hr className="dropdown-divider" /></li>
+
+                                    <li><a className="dropdown-item" href="#">
+                                        <img
+                                            style={{ marginRight: "10px" }}
+                                            src="https://www.apple.com/ac/globalnav/7/tr_TR/assets/ac-store/bag.svg" height="20" alt="" />
+                                        Çanta
+                                    </a>
+                                    </li>
+
+                                    <li><hr className="dropdown-divider" /></li>
+
+                                    <li><a className="dropdown-item" href="#">
+                                        <img
+                                            style={{ marginRight: "10px" }}
+                                            src="https://www.apple.com/ac/globalnav/7/tr_TR/assets/ac-store/savedbyyou.svg" height="20" alt="" />
+                                        Kaydedilen Ürünler
+                                    </a>
+                                    </li>
+
+                                    <li><hr className="dropdown-divider" /></li>
+
+                                    <li><a className="dropdown-item" href="#">
+                                        <img
+                                            style={{ marginRight: "10px" }}
+                                            src="https://www.apple.com/ac/globalnav/7/tr_TR/assets/ac-store/orders.svg" height="20" alt="" />
+                                        Siparişler
+                                    </a>
+                                    </li>
+
+                                    <li><hr className="dropdown-divider" /></li>
+
+                                    <li><a className="dropdown-item" href="#">
+                                        <img
+                                            style={{ marginRight: "10px" }}
+                                            src="https://www.apple.com/ac/globalnav/7/tr_TR/assets/ac-store/account.svg" height="20" alt="" />
+                                        Hesap
+                                    </a>
+                                    </li>
+
+                                    <li><hr className="dropdown-divider" /></li>
+
+                                    <li><a className="dropdown-item" href="#">
+                                        <img
+                                            style={{ marginRight: "10px" }}
+                                            src="https://www.apple.com/ac/globalnav/7/tr_TR/assets/ac-store/signIn.svg" height="20" alt="" />
+                                        Oturum açın
+                                    </a>
+                                    </li>
+
                                 </ul>
+
+
+
+
                             </div>
                         </li>
 
