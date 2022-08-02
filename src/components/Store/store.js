28e128490d86e7ea8ffccb0ed806a9e1c5cb9d4a
Ali Kartal <ali_kartal91@hotmail.com>
2022-08-02 16:22:39 +0300

    update

--- src/components/Store/store.js
+++ src/components/Store/store.js
@@ -5,7 +5,7 @@ import './store.css';
 
 function Store() {
     const [products, setProducts] = useState([]);
-    
+
     const URL = "http://localhost:3334/api/products";
 
     useEffect(() => {
@@ -21,39 +21,31 @@ function Store() {
     return (
         <div className='store'>
             <div className='container'>
+
                 <div className='row'>
-                <div className='mt-4 col-12 d-flex'>
-
                     {
-                        products.map((item, key) => (
-
-                        <div className="card col-4 ms-5 cards1">
-                            <div className="card-body row">
-                                <h5 className="card-title Title">{item.name}</h5>
-                                <div style={{ textAlign: "center" }} >
-                                    <img
-                                        src={item.image}
-                                        className="cardImg"
-                                        alt="..."
-                                        width="250"
-                                        height="250"
-                                    />
+                        products.map((item, index) => (
+                            <div className='mt-4 col-4 d-flex' key={index}>
+                                <div className="card col  ms-3 cards1">
+                                    <div className="card-body row">
+                                        <h5 className="card-title Title">{item.name}</h5>
+                                        <div style={{ textAlign: "center" }} >
+                                            <img
+                                                src={item.image}
+                                                className="cardImg"
+                                                alt="..."
+                                                width="250"
+                                                height="250"
+                                            />
+                                        </div>
+                                        <div className="card-text cardp col-8">Başlangıç fiyatı: {item.price} TL</div>
+                                        <button className='btn btn-primary buybutton col-4'>Sepete ekle</button>
+                                    </div>
                                 </div>
-                                    <div className="card-text cardp col-8">Başlangıç fiyatı: {item.price} TL</div>
-                                    <button className='btn btn-primary buybutton col-4'>Sepete ekle</button>
+
                             </div>
-                        </div>
-
-
-
-
-
-
-
-
-))
-}
-</div>
+                        ))
+                    }
 
                 </div>
             </div>
