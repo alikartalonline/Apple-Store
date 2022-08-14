8d69b0c8337369ff43139505a74b48739f3794c5
Ali Kartal <ali_kartal91@hotmail.com>
2022-08-14 23:34:01 +0300

    update

--- src/components/Basket/basket.js
+++ src/components/Basket/basket.js
@@ -11,50 +11,49 @@ function Basket() {
 
     return (
         <div>
-            {
-                // sepette ürün yok ise hata mesajı versin
-                items.length < 1 && (<div> You have not any items in your basket...</div>)
-            }
-
-            {
-                items.length > 0 &&
-                <>
-                    <ul className='ms-5 '>
-                        {
-                            items.map((item) => (
-
-                                <li
-                                    key={item.id}
-                                    style={{ marginBottom: 20 }}
-                                    className="mt-5 mb-5"
-                                >
+            <div className='container'>
+                <div className='row'>
 
 
-                                    <h4>{item.name}</h4>
-                                    <img src={item.image} height="100" alt="" className='d-flex' />
-                                    <div style={{fontSize:"20px", textAlign:"center"}}>{item.price}</div>
+                    {
+                        // sepette ürün yok ise hata mesajı versin
+                        items.length < 1 && (
+                        <div style={{textAlign:"center"}}> You have not any items in your basket...</div>)
+                    }
+
+                    {
+                        items.length > 0 &&
+                        <div className='col-8'>
+                                {
+                                    items.map((item, key) => (
+
+                                       <div className=''>
+                                            <div className='basket-item' key={item.id}>
+                                                <h4>{item.name}</h4>
+                                                <div className="d-flex">
+                                                    <img src={item.image} height="100" alt="" className='d-flex' />
+                                                    <div style={{ fontSize: "20px", marginTop: "20px", padding: "10px" }}>{item.price} TL</div>
+                                                </div>
+                                            </div>
 
 
-                                    <button
-                                        className='btn btn-danger'
-                                        onClick={() => removeFromBasket(item.id)}>
-                                        Remove from Basket
-                                    </button>
+                                            <button
+                                                className='btn btn-danger'
+                                                onClick={() => removeFromBasket(item.id)}>
+                                                Remove from Basket
+                                            </button>
+                                        </div>
+                                    ))
+                                }
+                        </div>
+                    }
 
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </>
-            }
+                    <div className='col-4 total-price'  >
+                            Toplam Fiyat: {total} TL
+                    </div>
 
-            <div mt="5" >
-                <div fontSize="22" >
-                    Total: {total} TL
                 </div>
             </div>
-
-
         </div>
     )
 }
